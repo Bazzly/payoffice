cf37a5e3d1a76d8873381832c77e764348115855
ALLI BAZEET <bazeet4love@gmail.com>
2022-12-06 17:38:35 +0100

    building

---
+++ src/Providers/PaymentServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Bazzlycodes\Payoffice\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PaymentServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
--- tests/Unit/InitialTest.php
+++ tests/Unit/InitialTest.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace Bazzlycodes\Payoffice\Tests;
+namespace Bazzlycodes\Payoffice\tests\Unit;
 
 use Orchestra\Testbench\TestCase;
 
 class InitialTest extends TestCase
 {
+    /** @test */
     public function my_test()
     {
         $this->assertTrue(true);
