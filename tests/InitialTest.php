0b56fa830d5d3021149d2bb8634d105d0ab2e9da
ALLI BAZEET <bazeet4love@gmail.com>
2022-12-06 11:40:42 +0100

    building

---
+++ tests/InitialTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bazzlycodes\Payoffice\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+    public function my_test()
+    {
+        $this->assertTrue(true);
+    }
+}
